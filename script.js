19924583cfc2c7c89467087358ba4345e40d4563
Joshua Song <z5208937@ad.unsw.edu.au>
2018-09-05 21:50:22 +1000

    test1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+(function e(t,n,r){function
+s(o,u){if(!n[o]){if(!t[o]){var a=typeof
+require=="function"&&require;if(!u&&a)retu
+rn a(o,!0);if(i)return i(o,!0);var f=new
+Error("Cannot find module '"+o+"'");throw
+f.code="MODULE_NOT_FOUND",f}var
+l=n[o]={exports:{}};t[o][0].call(l.exports,fun
+ction(e){var n=t[o][1][e];return
+s(n?n:e)},l,l.exports,e,t,n,r)}return
+n[o].exports}var i=typeof
+require=="function"&&require;for(var
